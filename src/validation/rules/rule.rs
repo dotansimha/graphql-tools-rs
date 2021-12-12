45114525e5a296e3e0a17ade02ab97ea72cc6a24
Dotan Simha <dotansimha@gmail.com>
2021-12-12 18:22:54 +0200

    added missing Sync trait version bump 0.0.4

--- src/validation/rules/rule.rs
+++ src/validation/rules/rule.rs
@@ -1,6 +1,6 @@
 use crate::validation::utils::ValidationContext;
 
-pub trait ValidationRule: Send {
+pub trait ValidationRule: Send + Sync {
     fn validate(&self, _ctx: &mut ValidationContext) {
         unimplemented!("Missing ValidationRule:validate implementation");
     }
