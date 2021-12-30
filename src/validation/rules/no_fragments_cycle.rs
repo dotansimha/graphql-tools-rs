15676d0c070cfd74ba1ba779836c1b5c5bf1842b
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-12-29 18:36:05 -0600

    somewhat works

--- src/validation/rules/no_fragments_cycle.rs
+++ src/validation/rules/no_fragments_cycle.rs
@@ -1,11 +1,9 @@
+// use graphql_parser::query::Selection;
+
 use super::ValidationRule;
-use crate::validation::utils::ValidationError;
-use crate::{
-	ast::QueryVisitor,
-	static_graphql::query::{FragmentDefinition, FragmentSpread},
-	validation::utils::ValidationContext,
-};
-use graphql_parser::query::Selection;
+use crate::static_graphql::query::{Document, FragmentDefinition, FragmentSpread, Selection};
+use crate::validation::utils::{ValidationError, ValidationErrorContext};
+use crate::{ast::QueryVisitor, validation::utils::ValidationContext};
 use std::collections::HashMap;
 
 /// No fragment cycles
@@ -20,22 +18,21 @@ struct NoFragmentsCycleHelper<'a> {
 	// Tracks already visited fragments to maintain O(N) and to ensure that cycles
 	// are not redundantly reported.
 	visited_fragments: HashMap<String, bool>,
-	// Array of AST nodes used to produce meaningful errors
-	spread_path: Vec<FragmentDefinition>,
 	// Position in the spread path
 	fragment_spreads: Vec<FragmentSpread>,
-	spread_path_index_by_name: HashMap<String, usize>,
-	validation_context: &'a mut ValidationContext,
+	spread_path_index_by_name: HashMap<String, Option<usize>>,
+	validation_context: &'a ValidationContext<'a>,
+	errors_context: ValidationErrorContext<'a>,
 }
 
 impl<'a> QueryVisitor<NoFragmentsCycleHelper<'a>> for NoFragmentsCycle {
-	fn enter_fragment_spread(
-		&self,
-		_node: &FragmentSpread,
-		_visitor_context: &mut NoFragmentsCycleHelper<'a>,
-	) {
-		_visitor_context.fragment_spreads.push(_node.clone());
-	}
+	// fn enter_fragment_spread(
+	// 	&self,
+	// 	_node: &FragmentSpread,
+	// 	_visitor_context: &mut NoFragmentsCycleHelper<'a>,
+	// ) {
+	// 	_visitor_context.fragment_spreads.push(_node.clone());
+	// }
 	// fn enter_fragment_definition(
 	// 	&self,
 	// 	_node: &FragmentDefinition,
@@ -45,20 +42,35 @@ impl<'a> QueryVisitor<NoFragmentsCycleHelper<'a>> for NoFragmentsCycle {
 	// 	detect_cycles(node, _visitor_context);
 	// 	false;
 	// }
-	fn leave_fragment_definition(
+	// fn leave_fragment_definition(
+	// 	&self,
+	// 	_node: &FragmentDefinition,
+	// 	_visitor_context: &mut NoFragmentsCycleHelper<'a>,
+	// ) {
+	// 	let node = _node.clone();
+	// 	detect_cycles(node, _visitor_context);
+	// 	false;
+	// }
+
+	fn leave_document(
 		&self,
-		_node: &FragmentDefinition,
+		_node: &Document,
 		_visitor_context: &mut NoFragmentsCycleHelper<'a>,
 	) {
-		let node = _node.clone();
-		detect_cycles(node, _visitor_context);
-		false;
+		_visitor_context
+			.validation_context
+			.fragments
+			.iter()
+			.for_each(|(fragment_name, fragment)| {
+				if !_visitor_context
+					.visited_fragments
+					.contains_key(fragment_name)
+				{
+					detect_cycles(fragment.clone(), _visitor_context);
+				}
+			});
 	}
 }
-
-// FragmentDefinition -> FragmentDefinitionNode
-// FragmentSpread -> FragmentSpreadNode
-// InlierFragment -> InlineFragmentNode
 
 // This does a straight-forward DFS to find cycles.
 // It does not terminate when a cycle was found but continues to explore
@@ -68,61 +80,98 @@ fn detect_cycles(fragment: FragmentDefinition, ctx: &mut NoFragmentsCycleHelper)
 		return;
 	}
 
+	// mark fragment as visited
 	ctx.visited_fragments.insert(fragment.name.clone(), true);
-	// let spreads = get_fragment_spreads(&fragment);
-	// get all the fragment spreads in the selection set
-	// let spreads: Vec<Selection<String>> = fragment.selection_set.items.iter().collect();
 
-	let spreads = ctx.fragment_spreads.clone();
+	// get all the fragment spreads for the current fragment
+	let spreads: Vec<FragmentSpread> = fragment
+		.selection_set
+		.items
+		.iter()
+		.map(|f| {
+			if let Selection::FragmentSpread(fragment_spread) = f {
+				Some(fragment_spread.clone())
+			} else {
+				None
+			}
+		})
+		.filter(|x| x.is_some())
+		.map(|x| x.unwrap())
+		.collect::<Vec<FragmentSpread>>();
 
 	if spreads.len() == 0 {
 		return;
 	}
 
 	ctx.spread_path_index_by_name
-		.insert(fragment.name.clone(), ctx.spread_path.len());
+		.insert(fragment.name.clone(), Some(spreads.len()));
 
 	for spread_node in spreads {
 		let spread_name = spread_node.fragment_name.clone();
 		let cycle_index = ctx.spread_path_index_by_name.get(spread_name.as_str());
-		ctx.spread_path.push(fragment.clone());
+		ctx.fragment_spreads.push(spread_node.clone());
 
-		if cycle_index == None {
-			let fragment_spread =
-				ctx.validation_context.fragments.get(spread_name.as_str());
-			if fragment_spread.is_some() {
-				detect_cycles(fragment_spread.unwrap().clone(), ctx);
+		match cycle_index {
+			Some(index) => match index {
+				Some(index) => {
+					let cycle_path = &ctx.fragment_spreads[0..index.clone()];
+					ctx.errors_context.report_error(ValidationError {
+						locations: cycle_path
+							.iter()
+							.map(|f| f.position.clone())
+							.collect(),
+						message: format!(
+									"Cannot spread fragment \"{}\" within itself.",
+									spread_name
+								),
+					})
+				}
+				None => {
+					let fragment_spread = ctx
+						.validation_context
+						.fragments
+						.get(spread_name.as_str());
+					if fragment_spread.is_some() {
+						detect_cycles(
+							fragment_spread.unwrap().clone(),
+							ctx,
+						);
+					}
+				}
+			},
+			None => {
+				let fragment_spread =
+					ctx.validation_context.fragments.get(spread_name.as_str());
+				if fragment_spread.is_some() {
+					detect_cycles(fragment_spread.unwrap().clone(), ctx);
+				}
 			}
-		} else {
-			ctx.validation_context.report_error(ValidationError {
-				locations: [ctx.spread_path[0].position.clone()].to_vec(),
-				message: format!(
-					"Fragment \"{}\" cannot be spread here as it would produce a cycle",
-					spread_name
-				),
-			})
 		}
-		ctx.spread_path.pop();
+
+		ctx.fragment_spreads.pop();
 	}
+	ctx.spread_path_index_by_name
+		.insert(fragment.name.clone(), None);
 }
 
 impl<'a> NoFragmentsCycleHelper<'a> {
-	fn new(validation_context: &'a mut ValidationContext) -> Self {
+	fn new(validation_context: &'a ValidationContext<'a>) -> Self {
 		NoFragmentsCycleHelper {
 			visited_fragments: HashMap::new(),
-			spread_path: Vec::new(),
 			spread_path_index_by_name: HashMap::new(),
 			fragment_spreads: Vec::new(),
-			validation_context,
+			validation_context: validation_context,
+			errors_context: ValidationErrorContext::new(validation_context),
 		}
 	}
 }
 
 impl ValidationRule for NoFragmentsCycle {
-	fn validate(&self, ctx: &mut ValidationContext) {
+	fn validate<'a>(&self, ctx: &ValidationContext) -> Vec<ValidationError> {
 		let operation = ctx.operation.clone();
 		let mut helper = NoFragmentsCycleHelper::new(ctx);
-		self.visit_document(&operation, &mut helper)
+		self.visit_document(&operation, &mut helper);
+		return helper.errors_context.errors;
 	}
 }
 
@@ -134,6 +183,39 @@ fn single_reference_is_valid() {
 	let errors = test_operation_with_schema(
 		"fragment fragA on Dog { ...fragB }
 		fragment fragB on Dog { name }",
+		TEST_SCHEMA,
+		&mut plan,
+	);
+
+	let mes = get_messages(&errors).len();
+	assert_eq!(mes, 0);
+}
+
+#[test]
+fn spreading_twice_is_not_circular() {
+	use crate::validation::test_utils::*;
+
+	let mut plan = create_plan_from_rule(Box::new(NoFragmentsCycle {}));
+	let errors = test_operation_with_schema(
+		"fragment fragA on Dog { ...fragB, ...fragB }
+		fragment fragB on Dog { name }",
+		TEST_SCHEMA,
+		&mut plan,
+	);
+
+	let mes = get_messages(&errors).len();
+	assert_eq!(mes, 0);
+}
+
+#[test]
+fn spreading_twice_indirectly_is_not_circular() {
+	use crate::validation::test_utils::*;
+
+	let mut plan = create_plan_from_rule(Box::new(NoFragmentsCycle {}));
+	let errors = test_operation_with_schema(
+		"fragment fragA on Dog { ...fragB, ...fragC }
+		fragment fragB on Dog { ...fragC }
+		fragment fragC on Dog { name }",
 		TEST_SCHEMA,
 		&mut plan,
 	);
