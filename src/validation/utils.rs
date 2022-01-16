addc753ec0975739ff56ad39ab2ecc0990ce2fcc
Dotan Simha <dotansimha@gmail.com>
2022-01-16 20:08:41 +0200

    more cleanup

--- src/validation/utils.rs
+++ src/validation/utils.rs
@@ -8,13 +8,6 @@ pub struct ValidationContext<'a> {
     pub schema: &'a schema::Document,
 }
 
-impl<'a> ValidationContext<'a> {
-    /// Utilities for getting a specific source schema definition by it's name
-    pub fn find_schema_definition_by_name(&self, name: String) -> Option<&schema::TypeDefinition> {
-        find_schema_definition_by_name(&self.schema, name)
-    }
-}
-
 pub struct ValidationErrorContext {
     pub errors: Vec<ValidationError>,
 }
@@ -27,59 +20,6 @@ impl ValidationErrorContext {
     pub fn report_error(&mut self, error: ValidationError) {
         self.errors.push(error);
     }
-}
-
-pub fn find_schema_definition_by_name(
-    schema: &schema::Document,
-    name: String,
-) -> Option<&schema::TypeDefinition> {
-    for definition in &schema.definitions {
-        match definition {
-            schema::Definition::TypeDefinition(type_definition) => match type_definition {
-                schema::TypeDefinition::Object(object) if object.name.eq(&name) => {
-                    return Some(type_definition)
-                }
-                schema::TypeDefinition::Scalar(object) if object.name.eq(&name) => {
-                    return Some(type_definition)
-                }
-                schema::TypeDefinition::Interface(object) if object.name.eq(&name) => {
-                    return Some(type_definition)
-                }
-                schema::TypeDefinition::InputObject(object) if object.name.eq(&name) => {
-                    return Some(type_definition)
-                }
-                schema::TypeDefinition::Enum(object) if object.name.eq(&name) => {
-                    return Some(type_definition)
-                }
-                schema::TypeDefinition::Union(object) if object.name.eq(&name) => {
-                    return Some(type_definition)
-                }
-                _ => {}
-            },
-            _ => {}
-        }
-    }
-
-    None
-}
-
-pub fn find_object_type_by_name(
-    schema: &schema::Document,
-    name: String,
-) -> Option<&schema::ObjectType> {
-    for definition in &schema.definitions {
-        match definition {
-            schema::Definition::TypeDefinition(type_definition) => match type_definition {
-                schema::TypeDefinition::Object(object) if object.name.eq(&name) => {
-                    return Some(object)
-                }
-                _ => {}
-            },
-            _ => {}
-        }
-    }
-
-    None
 }
 
 #[derive(Debug)]
